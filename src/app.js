eddb11b887e4a1c5dc54a511093363f986855bbc
esteban-elias <esteban.elias@proton.me>
2023-04-13 20:20:59 -0400

    Solucionado un problema con CORS

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import indexRouter from './routes/index.route.js';  // /index.js?
 const app = express();  
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({credentials: true, origin: 'http://localhost:5500'}));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
